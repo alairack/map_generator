1b6fa4e22e13d7aa7f62e304d055ca88de06fedc
alairack <1477115009@qq.com>
2021-10-09 14:45:37 +0800

    通过重构代码，添加了滚动条，窗口缩略时图片同时缩略，解决了图片间的空白与窗口初始化过小的问题

--- map_render.py
+++ map_render.py
@@ -1,34 +1,45 @@
+from PyQt5.QtWidgets import (QWidget, QLabel, QScrollArea, QApplication,
+                             QVBoxLayout, QMainWindow, QGridLayout)
+from PyQt5.QtCore import Qt
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtGui import QPixmap
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QScrollArea
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5 import QtCore
 
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
-        self.title = 'map_render'
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle(self.title)
+        self.scroll = QScrollArea()
+        self.widget = QWidget()
+        self.vbox = QVBoxLayout()
         self.gridlayout = QGridLayout()
         self.gridlayout.setContentsMargins(0, 0, 0, 0)
         self.gridlayout.setSpacing(0)
-        self.setLayout(self.gridlayout)
+        self.load_image()
+        self.vbox.addLayout(self.gridlayout)
+        self.widget.setLayout(self.vbox)
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setWidget(self.widget)
+        self.setCentralWidget(self.scroll)
+        self.setWindowTitle('map_render')
         self.show()
-        self.setFixedSize(self.gridlayout.sizeHint())
 
     def add_texture(self, image_path, layout_row, layout_column):
         pixmap = QPixmap(image_path)
         pixmap_2 = pixmap.scaled(50, 50, QtCore.Qt.KeepAspectRatio)
-        label = QLabel(self)
+        label = QLabel()
         label.setPixmap(pixmap_2)
+        label.setScaledContents(True)
         self.gridlayout.addWidget(label, layout_row, layout_column, 1, 1)
 
-
-def main(window):
-    with open('export.txt', "r") as f:
+    def load_image(self):
+        with open('export.txt', "r") as f:
             content = f.read()
             content = content.replace(" ", "")
             convert = ["grass", "soil", "blue_brick", "magma", "bird", "fish", "tree", "friend", "boss", "diamond"]
@@ -37,17 +48,15 @@ def main(window):
             for ch in content:
                 if ch != "\n":
                     ch = convert[int(ch)]
-                    window.add_texture("./texture/" + ch + ".png", y, x)
+                    self.add_texture("./texture/" + ch + ".png", y, x)
                     x = x + 1
                 else:
-                    y = y+1
+                    y = y + 1
                     x = 0
-
-    return 0
+        return 0
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    render_window = MainWindow()
-    main(render_window)
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
     sys.exit(app.exec_())
