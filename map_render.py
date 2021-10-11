e0533eeabecb66503b2c1a1ec5f5da7a335b4628
alairack <1477115009@qq.com>
2021-10-11 10:36:53 +0800

    增加了'选择贴图目录'和'重新生成图片'选项

--- map_render.py
+++ map_render.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import (QWidget, QLabel, QScrollArea, QMessageBox,
-                             QVBoxLayout, QMainWindow, QGridLayout)
+                             QVBoxLayout, QMainWindow, QGridLayout, QFileDialog)
 from PyQt5.QtCore import Qt
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtGui import QPixmap
@@ -11,9 +11,10 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowFlags(Qt.Widget)
+        self.setWindowTitle('map_render')
         self.initUI()
 
-    def initUI(self):
+    def initUI(self, folder_path="./texture/"):
         self.scroll = QScrollArea()
         self.widget = QWidget()
         self.vbox = QVBoxLayout()
@@ -21,6 +22,7 @@ class MainWindow(QMainWindow):
         self.gridlayout = QGridLayout()
         self.gridlayout.setContentsMargins(0, 0, 0, 0)
         self.gridlayout.setSpacing(0)
+        self.folder_path = folder_path             # 默认贴图目录
         self.load_image()
         self.vbox.addLayout(self.gridlayout)
         self.widget.setLayout(self.vbox)
@@ -38,13 +40,25 @@ class MainWindow(QMainWindow):
         self.save_image.setShortcut("Ctrl+S")
         self.save_image.triggered.connect(self.save)
         self.menu.addAction(self.save_image)
+        self.choose_folder = QtWidgets.QAction(self)
+        self.choose_folder.setObjectName("choose_folder")
+        self.choose_folder.setText("选择贴图目录")
+        self.choose_folder.setToolTip("选择贴图文件所存放的目录")
+        self.choose_folder.setEnabled(True)
+        self.choose_folder.triggered.connect(self.get_folder)
+        self.menu.addAction(self.choose_folder)
+        self.reload_image = QtWidgets.QAction(self)
+        self.reload_image.setObjectName("reload")
+        self.reload_image.setText("重新生成图片")
+        self.reload_image.setEnabled(True)
+        self.reload_image.triggered.connect(self.reload)
+        self.menu.addAction(self.reload_image)
         self.menuBar.addAction(self.menu.menuAction())
         self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.scroll.setWidgetResizable(True)
         self.scroll.setWidget(self.widget)
         self.setCentralWidget(self.scroll)
-        self.setWindowTitle('map_render')
         self.show()
 
     def add_texture(self, image_path, layout_row, layout_column):
@@ -63,14 +77,20 @@ class MainWindow(QMainWindow):
                 config = read_config()
                 x = 0
                 y = 0
-                for ch in content:
-                    if ch != "\n":
-                        ch = config[int(ch)]
-                        self.add_texture("./texture/" + ch + ".png", y, x)
-                        x = x + 1
-                    else:
-                        y = y + 1
-                        x = 0
+                try:                                       # 判断是否已选择贴图目录，如果没有，使用默认目录
+                    self.folder_path
+                except NameError or ValueError or AttributeError:
+                    self.folder_path = "./texture/"
+                finally:
+                    for ch in content:
+                        if ch != "\n":
+                            ch = config[int(ch)]
+                            self.add_texture(self.folder_path + ch + ".png", y, x)
+                            x = x + 1
+                        else:
+                            y = y + 1
+                            x = 0
+
         except FileNotFoundError:
             QMessageBox.critical(None, '错误', '读取输入文件(export.txt)失败！')
             sys.exit(3)
@@ -78,6 +98,17 @@ class MainWindow(QMainWindow):
     def save(self):
         self.widget.grab().save('map_save.png')
         QMessageBox.information(self, "状态", "已成功保存至当前目录！")
+
+    def get_folder(self):
+        listdir = QFileDialog(self)
+        listdir.setFileMode(QFileDialog.DirectoryOnly)
+        listdir.setDirectory("./")
+        if listdir.exec_():
+            self.folder_path = listdir.selectedFiles()[0] + "/"
+
+    def reload(self):
+        self.close()
+        self.initUI(self.folder_path)
 
 
 def read_config():
@@ -91,7 +122,7 @@ def read_config():
                 config.insert(int(line[0]), line[1:])
         return config
     except FileNotFoundError:
-        QMessageBox.critical(None, '警告', "未能成功读取配置文件(config.txt)!")
+        QMessageBox.critical(None, '错误', "未能成功读取配置文件(config.txt)!")
         sys.exit(4)
 
 
